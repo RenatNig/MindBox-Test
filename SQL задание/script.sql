d17eebb5a166b953918bdbd2bc611bcf4b23dff4
RenatNig <trackid788@gmail.com>
2023-03-21 19:18:48 +0300

    added create tables sql

--- SQL задание/script.sql
+++ SQL задание/script.sql
@@ -1,3 +1,26 @@
 select p.Name, c.Name from Products p 
 left join RelProductsCategories rel on rel.ProductId = p.Id 
 left join Categories c on rel.CategoryId = c.Id 
+
+
+
+CREATE TABLE MainDb.dbo.Categories (
+	Id int NOT NULL,
+	Name varchar(50) COLLATE Cyrillic_General_CI_AS NULL,
+	CONSTRAINT PK_Categories PRIMARY KEY (Id)
+);
+
+CREATE TABLE MainDb.dbo.Products (
+	Id int NOT NULL,
+	Name varchar(50) COLLATE Cyrillic_General_CI_AS NULL,
+	CONSTRAINT PK_Products PRIMARY KEY (Id)
+);
+
+CREATE TABLE MainDb.dbo.RelProductsCategories (
+	Id int NOT NULL,
+	ProductId int NOT NULL,
+	CategoryId int NOT NULL,
+	CONSTRAINT PK_RelProductsCategories PRIMARY KEY (Id),
+	CONSTRAINT CategoryId FOREIGN KEY (CategoryId) REFERENCES MainDb.dbo.Categories(Id),
+	CONSTRAINT ProductId FOREIGN KEY (ProductId) REFERENCES MainDb.dbo.Products(Id)
+);
